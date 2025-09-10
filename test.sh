3a5a047890ed1e6083931142cc87a49735fed6cf
zhuang999 <zhuangde999@gmail.com>
2025-09-10 14:34:56 +0800

    init

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 nohup python eval_vllm.py --model_path ./Llama-3.1-8B  --dataset_name nyc --output_dir ./restart/checkpoint-7000 --test_file "datasets/NYC/data/test_codebook_origin.json" > restart/test-7000-1.txt 2>&1 &  
+CUDA_VISIBLE_DEVICES=0 nohup python eval_vllm.py --model_path ./Llama-3.1-8B  --dataset_name nyc --output_dir ./restart/checkpoint --test_file "datasets/NYC/data/test_codebook_origin.json" > restart/test.txt 2>&1 &  
 
